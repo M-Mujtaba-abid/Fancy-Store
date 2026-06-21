97857a27bb94d60f50c35a1b36b3ba6a740f58e1
Mujtaba Abid <mujtabaabid06@gmail.com>
2026-06-21 13:43:34 +0500

    create model for product variants

---
+++ ecommerce-backend/models/productVariant.model.js
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+class ProductVariant extends Model { }
+
+ProductVariant.init(
+    {
+        productId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        materialName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // ✅ Yeh Enum wala hi kaam karega, lekin bina database strictness ke
+                isIn: {
+                    args: [["Silver Coated", "Black Coated", "PVC + Cotton", "Micro Fiber"]],
+                    msg: "Invalid material quality selected"
+                }
+            }
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false, // Is quality ki price (e.g. 4497)
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            defaultValue: 50,
+        }
+    },
+    {
+        sequelize,
+        modelName: "ProductVariant",
+    }
+);
+
+export default ProductVariant;
